d52b8bc39456825757bca031ce6511bb46b986a6
mhkasif <haseebasif97@gmail.com>
2020-11-06 04:20:10 +0500

    back button bug removed

--- src/Components/BackButton/BackButton.js
+++ src/Components/BackButton/BackButton.js
@@ -1,60 +1,65 @@
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import React from "react";
 import "./BackButton.scss";
 
 const BackButton = () => {
   const textContainer = {
-      initial:{
-y:0
-      },
-      hover:{
-        // y:10,
-        transition: {
-            staggerChildren:0.4,
-            delayChildren:1.6,
-            staggerDirection:-1
-        }
+    // animate: {
+    //   //   x: "20px",
+    //   opacity: 0,
+    //   transition: {
+    //     delayChildren: 0.6,
+    //     staggerChildren: 0.3,
+    //     staggerDirection: 1,
+    //     // repeatType: "reverse",
+    //     // repeat:1
+    //   },
+    // },
+hov:{
+    y:0,
+    transition: {
+        staggerChildren: 0.03,
+        delayChildren: .01,
+        staggerDirection:-1,
+
     },
-    }
+},
+    hidden: { opacity:1},
+    show: {
+        opacity:1,
+        transition: {
+            staggerChildren: 0.1,
+            delayChildren: 1.8,
+        },
+    },
+  };
 
   const firstText = {
-      initial: {
-        y: 0,
-      },
-      hover:{
-          y:-10,
-        transition: {
-          duration: 2.4,
-          ease: "easeIn",
-        delay:2
 
-        }
-      },
-    //   animate:{
+    hidden: { opacity:0 },
+    show: { opacity:1 },
+    hov: { opacity:0,
+    transition:{
+        repeat:1,
+        repeatType:'reverse'
+    } },
+}
 
-    //     transition: {
-    //         duration: 2.4,
-    //         ease: "easeIn",
-    //       delay:2
+const itemB = {
+    hidden: { opacity:0 },
+    show: { opacity:1 },
+    hov: { y:-10 },
+}
 
-    //       }
-    //         }
-
-  };
-  const secondText = {
-    secondText: {
-      animate: {
-        y: 10,
-      },
-      transistion: {
-        duration: 1,
-        ease: "linear",
-      },
-    },
-  };
 
   return (
-    <div className="back-button">
+    <motion.div
+      initial="initial"
+      animate="animate"
+      hover="hover"
+      exit="exit"
+      className="back-button"
+    >
       <motion.div
         exit={{ width: "0px" }}
         initial={{ width: "0px" }}
@@ -63,24 +68,130 @@ y:0
         className="back-button-line"
       ></motion.div>
 
-      <motion.div animate='animate' initial='initial'  whileHover="hover" variants={textContainer} className="back-button-text-container">
-        {/* <motion.div className="firstText"> */}
-          <motion.span  initial="initial" variants={firstText}>S</motion.span>
-          <motion.span  initial="initial" variants={firstText}>l</motion.span>
-          <motion.span  initial="initial" variants={firstText}>i</motion.span>
-          <motion.span  initial="initial" variants={firstText}>d</motion.span>
-          <motion.span  initial="initial" variants={firstText}>e</motion.span>
-          &nbsp;
-        {/* </motion.div> */}
-        {/* <div className="secondText"> */}
-          <motion.span  initial="initial" variants={firstText}>B</motion.span>
-          <motion.span  initial="initial" variants={firstText}>a</motion.span>
-          <motion.span  initial="initial" variants={firstText}>c</motion.span>
-          <motion.span  initial="initial" variants={firstText}>k</motion.span>
-        {/* </div> */}
+      <motion.div
+      animate='animate'
+        variants={textContainer}
+                initial="hidden"
+                animate="show"
+                whileHover='hov'
+        className="back-button-text-container"
+      >
+        <motion.span key={"a"}  variants={firstText}>
+          S
+        </motion.span>
+        <motion.span key={"b"} variants={firstText}>
+          l
+        </motion.span>
+        <motion.span key={"c"} variants={firstText}>
+          i
+        </motion.span>
+        <motion.span key={"d"} variants={firstText}>
+          d
+        </motion.span>
+        <motion.span key={"e"} variants={firstText}>
+          e&nbsp;
+        </motion.span>
+        <motion.span key={"f"} variants={firstText}>
+          B
+        </motion.span>
+        <motion.span key={"g"} variants={firstText}>
+          a
+        </motion.span>
+        <motion.span key={"h"} variants={firstText}>
+          c
+        </motion.span>
+        <motion.span key={"i"} variants={firstText}>
+          k
+        </motion.span>
       </motion.div>
-    </div>
+    </motion.div>
   );
 };
 
 export default BackButton;
+// export default function BackButton() {
+//     const container = {
+//         hidden: { rotate: 90 },
+//         show: {
+//             rotate: 0,
+//             transition: {
+//                 staggerChildren: 0.1,
+//                 delayChildren: 0.3,
+//             },
+//         },
+//     }
+
+//     const itemA = {
+//         hidden: { scale: 0, top: 100 },
+//         show: { scale: 1, top: 30 },
+//     }
+
+//     const itemB = {
+//         hidden: { scale: 0, top: 200 },
+//         show: { scale: 1, top: 80 },
+//     }
+
+//     const background = "#80F"
+
+//     return (
+
+//             <motion.div
+//                 style={{
+//                     width: 150,
+//                     height: 150,
+//                     borderRadius: 30,
+//                     backgroundColor: "#fff",
+//                     position: "relative",
+//                 }}
+//                 variants={container}
+//                 initial="hidden"
+//                 animate="show"
+//             >
+//                 <motion.div
+//                     style={{
+//                         width: 40,
+//                         height: 40,
+//                         borderRadius: 75,
+//                         backgroundColor: background,
+//                         position: "absolute",
+//                         left: 30,
+//                     }}
+//                     variants={itemA}
+//                 />
+//                 <motion.div
+//                     style={{
+//                         width: 40,
+//                         height: 40,
+//                         borderRadius: 75,
+//                         backgroundColor: background,
+//                         position: "absolute",
+//                         left: 80,
+//                     }}
+//                     variants={itemA}
+//                 />
+//                 <motion.div
+//                     style={{
+//                         width: 40,
+//                         height: 40,
+//                         borderRadius: 75,
+//                         backgroundColor: background,
+//                         position: "absolute",
+//                         left: 30,
+//                     }}
+//                     variants={itemB}
+//                 />
+//                 <motion.div
+//                     style={{
+//                         width: 40,
+//                         height: 40,
+//                         borderRadius: 75,
+//                         backgroundColor: background,
+//                         position: "absolute",
+//                         left: 80,
+//                     }}
+//                     variants={itemB}
+//                 />
+//             </motion.div>
+
+//     )
+// }
