35427340b19c9d1e62e9cd34f9d0112fb7b2c6f0
mhkasif <mhaseeb@maavan.com>
2021-06-01 23:39:57 +0500

    Updates

---
+++ precache-manifest.40ec3171d5da57d33541270d16e13004.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6c00059dfb7df1fae2e7e7c9a552b03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e140e056837d794a0490",
+    "url": "/static/css/main.d29463ca.chunk.css"
+  },
+  {
+    "revision": "f848748d410c0c7797d8",
+    "url": "/static/js/2.1d324241.chunk.js"
+  },
+  {
+    "revision": "640225f8ae7c2fabfcb7717eb5dde715",
+    "url": "/static/js/2.1d324241.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e140e056837d794a0490",
+    "url": "/static/js/main.d2fe3c54.chunk.js"
+  },
+  {
+    "revision": "30aa5163c538eaf9052e",
+    "url": "/static/js/runtime-main.ca0c0ddc.js"
+  },
+  {
+    "revision": "a5310d2b6783311487da997cd8c99ec1",
+    "url": "/static/media/Group1.a5310d2b.png"
+  },
+  {
+    "revision": "3558ef4a37ab98f4f0b960570ef4b2ac",
+    "url": "/static/media/mcdonalds-bg.3558ef4a.jpg"
+  },
+  {
+    "revision": "d1ce74bbfbab632c7f10e6862d14ff2d",
+    "url": "/static/media/new-tab.d1ce74bb.png"
+  },
+  {
+    "revision": "bf6d88dfa9be56f0c2f5e9e2ad4e62ff",
+    "url": "/static/media/slectus-macbook.bf6d88df.png"
+  }
+]);
