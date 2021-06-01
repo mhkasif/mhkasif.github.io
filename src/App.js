158e09c2a447dc34c350387c6e4055ff275129c2
mhkasif <mhaseeb@maavan.com>
2021-06-01 23:24:03 +0500

    svg added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { AnimatePresence } from "framer-motion";
 import React from "react";
 import { Route, Switch, useLocation } from "react-router-dom";
 import "./App.css";
-import CircleSvg from "./Components/CircleSvg/CircleSvg";
 import Home from "./Components/Home/Home";
 import Hkjobz from "./pages/Hkjobz/Hkjobz";
 import Slectus from "./pages/Slectus/Slectus";
@@ -12,7 +11,7 @@ function App() {
   const location = useLocation();
   return (
     <div>
-<AnimatePresence initial={true} exitBeforeEnter>
+      <AnimatePresence initial={true} exitBeforeEnter>
         <Switch location={location} key={location.pathname}>
           <Route key="a" exact path="/" component={Home} />
           <Route key="b" exact path="/hkjobz" component={Hkjobz} />
