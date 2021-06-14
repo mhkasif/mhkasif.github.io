1247db3af1185f404c86732604664800673182c9
mhkasif <haseebasif97@gmail.com>
2021-06-14 13:46:17 +0500

    scroll attached on page count

--- src/Components/Home/Content/Content.js
+++ src/Components/Home/Content/Content.js
@@ -100,22 +100,25 @@
 import React, { Component, useCallback, useEffect } from "react";
 import { connect } from "react-redux";
 import { TimelineLite, Power3 } from "gsap";
-import { decrementCounter } from "../../../Redux/Actions/HomeComponentInfoActions/HomeComponentInfoActions";
+import { decrementCounter, setScrollCounter } from "../../../Redux/Actions/HomeComponentInfoActions/HomeComponentInfoActions";
 
 import Footer from "./Footer/Footer";
 import SlectusHome from "./SlectusHome/SlectusHome";
 import HkjobzHome from "./HkjobzHome/HkjobzHome";
 import AboutHome from "./AboutHome/AboutHome";
 import ProjectHome from "./ProjectsHome/ProjectHome";
-
-const Content = ({ scrollCounter }) => {
+import { Timeline } from "gsap/gsap-core";
+import '../../../CSS/Content.scss'
+import gsap from "gsap/gsap-core";
+const Content = ({ scrollCounter,setScrollCounter }) => {
   const isFirstRender = React.useRef(true);
   useEffect(() => {
     console.log(scrollCounter);
     if (!isFirstRender.current) {
       const tl = new TimelineLite({});
-      tl.to(
-        ".page-number-up",
+      tl.to('.non-active',{display:"none"},0)
+      .to(
+        ".active",
         1,
         {
           y: "-100%",
@@ -139,7 +142,7 @@ const Content = ({ scrollCounter }) => {
           0
         )
         .to(
-          ".page-number-up",
+          ".active",
           1,
           {
             y: "0",
@@ -168,20 +171,23 @@ const Content = ({ scrollCounter }) => {
     isFirstRender.current = false;
     // var { l1, l2, l3, l4 } = this.refs;
     var tl = new TimelineLite({
-      delay: 0.5,
     });
     // tl.to(l1, 1, { scale:(1), ease: Power3.easeOut })
     //   .to(l2, 1, {scale:(1), ease: Power3.easeOut }, 0)
     //   .to(l3, 1, { scale:(1), ease: Power3.easeOut }, 0)
     //   .to(l4, 1, { scale:(1), ease: Power3.easeOut }, 0);
-    tl.to(
-      ".page-number-up",
+    tl.set('.active',{
+      y:-100,
+      opacity:0
+    })
+    .to(
+      ".active",
       1,
       {
         y: 0,
         opacity: 1,
       },
-      0
+
     )
       .to(
         ".page-number-down",
@@ -273,6 +279,33 @@ const Content = ({ scrollCounter }) => {
 
   // render() {
   //   var { scrollCounter } = this.props;
+  const hover=()=>{
+    const tl=new Timeline()
+    tl.to('.page-number-up',{
+      display:"block",
+      ease:Power3.easeIn,
+      duration: .5
+    })
+  }
+  const mouseOver=(cname)=>{
+    console.log(cname)
+    gsap.to('.overflow-wrapper', {
+            ease:Power3.easeInOut,
+      scrollTo:{
+      y:cname,
+      offsetY:20
+
+    }});
+
+  }
+  const leave=()=>{
+    const tl=new Timeline()
+    tl.to('.non-active',{
+      display:"none",
+      ease:Power3.easeOut,
+      duration:.5
+    })
+  }
   return (
     <div className={`contentContainer ${scrollCounter === 5 ? "blue-bg" : ""}`}>
       {scrollCounter === 5 && <div className="background-ball"></div>}
@@ -285,8 +318,16 @@ const Content = ({ scrollCounter }) => {
               "rgba(255,255,255,0.7)",
           }}
         >
-          <div className="overflow-wrapper">
-            <div className="page-number page-number-up">{scrollCounter}</div>
+          <div onMouseLeave={leave} onMouseOver={hover} className="overflow-wrapper ">
+          <div   className="page-number-up-container page-number-overflow-wrapper">
+          <div onClick={()=>setScrollCounter(1)} onMouseOver={()=>mouseOver('#page-number-up-1')} id="page-number-up-1"  className={`page-number page-number-up page-number-up-1 ${scrollCounter===1?'active':"non-active"}`} style={{display:scrollCounter===1?"block":"none"}}>1</div>
+          <div onClick={()=>setScrollCounter(2)} onMouseOver={()=>mouseOver('#page-number-up-2')} id="page-number-up-2"  className={`page-number page-number-up page-number-up-2 ${scrollCounter===2?'active':"non-active"}`} style={{display:scrollCounter===2?"block":"none"}}>2</div>
+          <div onClick={()=>setScrollCounter(3)} onMouseOver={()=>mouseOver('#page-number-up-3')} id="page-number-up-3"  className={`page-number page-number-up page-number-up-3 ${scrollCounter===3?'active':"non-active"}`} style={{display:scrollCounter===3?"block":"none"}}>3</div>
+          <div onClick={()=>setScrollCounter(4)} onMouseOver={()=>mouseOver('#page-number-up-4')} id="page-number-up-4"  className={`page-number page-number-up page-number-up-4 ${scrollCounter===4?'active':"non-active"}`} style={{display:scrollCounter===4?"block":"none"}}>4</div>
+          <div onClick={()=>setScrollCounter(5)} onMouseOver={()=>mouseOver('#page-number-up-5')} id="page-number-up-5"  className={`page-number page-number-up page-number-up-5 ${scrollCounter===5?'active':"non-active"}`} style={{display:scrollCounter===5?"block":"none"}}>5</div>
+          <div onClick={()=>setScrollCounter(6)} onMouseOver={()=>mouseOver('#page-number-up-6')} id="page-number-up-6"  className={`page-number page-number-up page-number-up-6 ${scrollCounter===6?'active':"non-active"}`} style={{display:scrollCounter===6?"block":"none"}}>6</div>
+
+          </div>
           </div>
 
           <div
@@ -388,6 +429,8 @@ var mapStateToProps = (state) => ({
 
 var actions = {
   decrementCounter,
+  setScrollCounter
+
 };
 
 export default connect(mapStateToProps, actions)(Content);
